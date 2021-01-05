2313b3606b636ce3a661da5b2277c22a26480851
liangyouwen <layouwen@gmail.com>
2021-01-05 20:58:31 +0800

    add index icon

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -6,38 +6,51 @@
     </view>
     <view class='tab-wrapper'>
       <block v-for='(tab, index) in tabsList' :key='index'>
-        <view class='tab' :class='active === index ? "active" : ""' @click='onChangeTab(index)'>{{ tab }}</view>
+        <view class='tab' :class='tabActive === index ? "active" : ""' @click='onChangeTab(index)'>{{ tab }}</view>
       </block>
     </view>
-    <view class='item-wrapper'>
-      <view class='item' v-for='(item,index) in [1,2,3]' :key='index'>
-        <image class='item-icon' src='' />
-        <text class='item-title'>腾讯视频</text>
-        <button class='item-btn'>低至6元起</button>
+    <block v-for="(content, index) in contents" :key="index">
+      <view class='item-wrapper' v-show="tabActive === index">
+        <view class='item'>
+          <image class='item-icon' src='/static/tengxun.png' />
+          <text class='item-title'>腾讯视频</text>
+          <button class='item-btn'>低至{{ index }}元起</button>
+        </view>
+        <view class='item'>
+          <image class='item-icon' src='/static/aiqiyi.png' />
+          <text class='item-title'>爱奇艺</text>
+          <button class='item-btn'>查看全部套餐</button>
+        </view>
+        <view class='item'>
+          <image class='item-icon' src='/static/youku.png' />
+          <text class='item-title'>优酷</text>
+          <button class='item-btn'>查看全部套餐</button>
+        </view>
       </view>
-    </view>
+    </block>
   </view>
 </template>
 
 <script>
-	export default {
-    data () {
-      return {
-        list: [
-          'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fa4.att.hudong.com%2F27%2F67%2F01300000921826141299672233506.jpg&refer=http%3A%2F%2Fa4.att.hudong.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1612432022&t=0b05f3910384274c525037b8802423c5',
-          'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fa2.att.hudong.com%2F27%2F81%2F01200000194677136358818023076.jpg&refer=http%3A%2F%2Fa2.att.hudong.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1612432022&t=415820e45c02a5577db3d3da7d5b0d8a'
-        ],
-        tabsList: ['影视会员', '音频会员', '阅读会员'],
-        active: 0
-      }
-    },
-    onLoad () { },
-    methods: {
-      onChangeTab (index) {
-        this.active = index
-      }
+export default {
+  data () {
+    return {
+      list: [
+        'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fa4.att.hudong.com%2F27%2F67%2F01300000921826141299672233506.jpg&refer=http%3A%2F%2Fa4.att.hudong.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1612432022&t=0b05f3910384274c525037b8802423c5',
+        'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fa2.att.hudong.com%2F27%2F81%2F01200000194677136358818023076.jpg&refer=http%3A%2F%2Fa2.att.hudong.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1612432022&t=415820e45c02a5577db3d3da7d5b0d8a'
+      ],
+      contents: [1, 2, 3],
+      tabsList: ['影视会员', '音频会员', '阅读会员'],
+      tabActive: 0
+    }
+  },
+  onLoad () { },
+  methods: {
+    onChangeTab (index) {
+      this.tabActive = index
     }
   }
+}
 </script>
 
 <style lang='scss' scoped>
@@ -104,12 +117,17 @@
       background: #FFF;
       box-shadow: 0 rpx(5) rpx(15) 0 rgba(45, 47, 64, 0.05);
       border-radius: rpx(10);
+      &:not(:first-child) {
+        margin-top: rpx(25);
+      }
       > .item-icon {
-        width: rpx(90);
-        height: rpx(90);
+        width: rpx(110);
+        height: rpx(110);
+        //width: rpx(90);
+        //height: rpx(90);
         margin-right: rpx(39);
         background: #FFF;
-        box-shadow: 0 rpx(8) rpx(15) 0 rgba(16, 171, 242, 0.21);
+        //box-shadow: 0 rpx(8) rpx(15) 0 rgba(16, 171, 242, 0.21);
         border-radius: rpx(16);
       }
       > .item-title {
@@ -123,7 +141,7 @@
         height: rpx(68);
         line-height: rpx(68);
         padding: 0;
-        font-size: rpx(30);
+        font-size: rpx(28);
         font-weight: 500;
         color: #4D321B;
         background: linear-gradient(75deg, #FBE8C8 0%, #E5BE7B 100%);
