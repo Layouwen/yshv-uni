33d7dfb168406529d41e561f9a2d1ce5713d1c44
layouwen <layouwen@gmail.com>
2021-01-05 09:35:30 +0800

    create memberrechrge page router

---
+++ src/pages/memberrecharge/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <view class='container'>1</view>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<style lang='scss' scoped>
+@import "~@/assets/styles/fn.scss";
+
+// 所有rpx单位写法换成 rpx(数字)
+// 例如： width: rpx(100);
+.container {
+
+}
+</style>
